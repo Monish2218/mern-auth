17e1feff438f711085efdcf7f82a92b154574d91
Monish Agrawal <monish.21910923@viit.ac.in>
2024-02-19 19:43:21 +0530

    create header component

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,17 +4,19 @@ import About from './pages/About'
 import SignIn from './pages/SignIn'
 import SignUp from './pages/SignUp'
 import Profile from './pages/Profile'
+import Header from './components/Header'
 
 export default function App() {
   return (
     <BrowserRouter>
-        <Routes>
-            <Route path='/' element={<Home />} />
-            <Route path='/about' element={<About />} />
-            <Route path='/sign-in' element={<SignIn />} />
-            <Route path='/sign-up' element={<SignUp />} />
-            <Route path='/profile' element={<Profile />} />
-        </Routes>
+      <Header />
+      <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/about' element={<About />} />
+          <Route path='/sign-in' element={<SignIn />} />
+          <Route path='/sign-up' element={<SignUp />} />
+          <Route path='/profile' element={<Profile />} />
+      </Routes>
     </BrowserRouter>
   )
 }
