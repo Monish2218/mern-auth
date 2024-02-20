b675cb43f2bc8f7d06ff8dee8f7f268d5f58b1f8
Monish Agrawal <monish.21910923@viit.ac.in>
2024-02-20 13:03:41 +0530

    create signup api route

--- api/index.js
+++ api/index.js
@@ -1,6 +1,8 @@
 import express from 'express';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
+import userRoutes from './routes/user.routes.js';
+import authRoutes from './routes/auth.routes.js';
 
 dotenv.config();
 
@@ -15,6 +17,11 @@ mongoose
 
 const app = express();
 
+app.use(express.json());
+
 app.listen(3000, () => {
     console.log('Server listening on port 3000!');
 });
+
+app.use('/api/user', userRoutes);
+app.use('/api/auth', authRoutes);
